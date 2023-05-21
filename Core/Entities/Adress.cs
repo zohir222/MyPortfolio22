cb75549fe168dec4fb50de5886834412e0c9d852
zohir222 <bensalemzohir284@gmail.com>
2023-05-21 12:21:41 +0100

    Add project files.

---
+++ Core/Entities/Adress.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Entities
+{
+    public class Adress : EntityBase
+    {
+        public string Street { get; set; }
+
+        public string City { get; set; }
+
+        public string Number { get; set; }
+
+    }
+}
---
+++ Core/Entities/Owner.cs
@@ -0,0 +1,14 @@
+﻿namespace Core.Entities
+{
+    public class Owner : EntityBase
+    {
+        public string FullName { get; set; }
+
+        public string Profil { get; set; }
+
+        public string Avatar { get; set; }
+
+        public Adress Adress { get; set; }
+
+    }
+}
